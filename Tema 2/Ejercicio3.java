401f4a702ca4f700b3973dc30209db7e0a90c055
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-09-27 08:29:14 +0200

    Añadido los ejercicios ejercicios de los temas correspondientes

---
+++ Tema 2/Ejercicio3.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 3
+ *
+ * @author Juan Luis Aranda
+ */
+
+public class UsoDeStringsEj3 {
+  public static void main(String[] args) {
+    String nombre = "Juan Luis Aranda Navarro";
+    String direccion = "C/ José Calderón";
+    int telefono = 622251865;
+  
+    System.out.println(nombre);
+    System.out.println(direccion);
+    System.out.println(telefono);
+  }
+}
