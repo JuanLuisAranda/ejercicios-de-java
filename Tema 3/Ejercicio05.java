ffecdcb38147ac1fb58e653617dfce41335ee172
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-09-30 20:17:04 +0200

    Relación de Ejercicios del Tema 3

--- Tema 3/Ejercicio05.java
+++ Tema 3/Ejercicio05.java
@@ -4,7 +4,7 @@
  * @author Juan Luis Aranda
  */
 
-public class Ejercicio04 {
+public class Ejercicio05 {
   public static void main(String[] args) {
     
 	System.out.println("Calculadora de áreas de rectángulos.");
