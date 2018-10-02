02fcb9612ec31ab2f81dcb3da556d67654aa8bfd
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-10-02 13:38:28 +0200

    Relación de Ejercicios del Tema 4

---
+++ Tema 4/Ejercicio02.java
@@ -0,0 +1,29 @@
+/**
+ * Ejercicio 2
+ *
+ * @author Juan Luis Aranda
+ */
+
+public class Ejercicio02 {
+  public static void main(String[] args) {
+
+    System.out.print("Por favor, introduzca una hora del día excluyendo los minutos y segundos: ");
+    int hora = Integer.parseInt(System.console().readLine());
+    
+    if ((hora >= 6) && (hora <= 12)) {
+      System.out.println("¡Buenos días!");
+    }
+        
+    if ((hora >= 13) && (hora <= 20)) {
+      System.out.println("¡Buenas tardes!");
+    }
+      
+    if (((hora >= 21) && (hora < 24)) || ((hora <= 5) && (hora >= 0))) {
+      System.out.println("!Buenas noches!");
+    }
+      
+    if ((hora >= 24) || (hora < 0)) {
+      System.out.println("Esa hora no existe.");
+    }
+  }
+}
