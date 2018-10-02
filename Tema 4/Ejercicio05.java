fd96f5eabc87fd964e09c76150cde1ec49c07915
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-10-02 14:15:39 +0200

    Relación de Ejercicios del Tema 4

---
+++ Tema 4/Ejercicio05.java
@@ -0,0 +1,22 @@
+/**
+ * Ejercicio 5
+ *
+ * @author Juan Luis Aranda
+ */
+
+public class S04Ejercicio05 {
+  public static void main(String[] args) {
+
+    System.out.println("Calculadora de ecuaciones de primer grado del tipo ax + b = 0");
+    System.out.print("Por favor, introduzca el valor de a: ");
+    Double a = Double.parseDouble(System.console().readLine());
+    System.out.print("Ahora introduzca el valor de b: ");
+    Double b = Double.parseDouble(System.console().readLine());
+
+    if (a == 0) {
+      System.out.println("Esa ecuación no tiene solución real.");
+    } else {
+      System.out.println("x = " + (-b/a));
+    }
+  }
+}
