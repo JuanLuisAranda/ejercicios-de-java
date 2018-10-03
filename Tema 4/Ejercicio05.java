851a8a5e5135ea46c93d33df39cfa77ad86dac8e
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-10-03 08:59:55 +0200

    Relación de Ejercicios del Tema 4

--- Tema 4/Ejercicio05.java
+++ Tema 4/Ejercicio05.java
@@ -4,7 +4,7 @@
  * @author Juan Luis Aranda
  */
 
-public class S04Ejercicio05 {
+public class Ejercicio05 {
   public static void main(String[] args) {
 
     System.out.println("Calculadora de ecuaciones de primer grado del tipo ax + b = 0");
