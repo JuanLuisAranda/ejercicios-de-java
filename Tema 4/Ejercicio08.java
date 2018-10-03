fbd70aaf965b9854e4092c06387664c611ae85d4
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-10-03 10:31:42 +0200

    Relación de Ejercicios del Tema 4

--- Tema 4/Ejercicio08.java
+++ Tema 4/Ejercicio08.java
@@ -3,7 +3,7 @@
  *
  * @author Juan Luis Aranda
  */
-public class Ejercicio07 {
+public class Ejercicio08 {
 
   public static void main(String[] args) {
 	
@@ -22,5 +22,26 @@ public class Ejercicio07 {
     double media = (nota1 + nota2 + nota3) / 3;
     
     System.out.printf("La media es %.2f\n", media);
+    
+      if (media < 5) {
+      System.out.print("Insuficiente");
+    }
+    
+    if ((media >= 5) && (media < 6)) {
+      System.out.print("Suficiente");
+    }
+    
+    if ((media >= 6) && (media < 7)) {
+      System.out.print("Bien");
+    }
+    
+    if ((media >= 7) && (media < 9)) {
+      System.out.print("Notable");
+    }
+    
+    if (media >= 9) {
+      System.out.print("Sobresaliente");
+    }
+		
+    }
   }
-}
