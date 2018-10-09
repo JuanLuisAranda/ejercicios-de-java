510bc8740d1301ad6ad3c53c6a0d8d0df18def17
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-10-09 13:54:00 +0200

    Relación de Ejercicios del Tema 5

---
+++ Tema 5/Ejercicio01.java
@@ -0,0 +1,14 @@
+/**
+ * Ejercicio 1
+ * 
+ * @author Juan Luis Aranda
+ */
+public class Ejercicio01 {
+
+  public static void main(String[] args) {
+    
+    for(int i = 0; i <= 100; i += 5) {
+      System.out.println(i);
+    }
+  }
+}
