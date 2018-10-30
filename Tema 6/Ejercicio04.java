5567d5ce6f21141b85accef47d54be35cda4c4f6
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-10-30 14:10:25 +0100

    Relación de Ejercicios del Tema 6

---
+++ Tema 6/Ejercicio04.java
@@ -0,0 +1,17 @@
+package tema06;
+
+/**
+ * 
+ * Ejercicio 4
+ *
+ * @author Juan Luis Aranda
+ */
+public class Ejercicio04 {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 20; i++) {
+System.out.print((int)(Math.random()*10) + " ");
+    }
+System.out.println();
+    
+  }
+}
