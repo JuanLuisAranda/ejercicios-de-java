d081286cd352c0e577d780148e10b1605d750a2e
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2018-11-07 08:32:32 +0100

    Relación de Ejercicios del Tema 7

---
+++ Tema 7/Ejercicio01.java
@@ -0,0 +1,35 @@
+package tema07;
+
+/**
+ * 
+ * Ejercicio 1
+ *
+ * @author Juan Luis Aranda
+ */
+public class Ejercicio01 {
+  public static void main(String[] args) {
+    //Defino el array 
+	int[] num; 
+    /////////////////
+    
+    //Reservo espacios en el array
+	num = new int[11]; 
+    /////////////////
+    
+    //Defino los huecos del array
+	num[0] = 39;
+	num[1] = -2;
+	num[4] = 0;
+	num[6] = 14;
+	num[8] = 5;
+	num[9] = 120;		
+    /////////////////
+    
+    //Muestro por pantalla utilizando un bucle for
+	for(int i = 0 ; i < 11; i++){
+		System.out.print(num[i]+"  ");
+	}
+/////////////////
+  }
+  
+}
