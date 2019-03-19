24bb90173b060aec1450a0aa4edeb3b1718f059a
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2019-03-19 10:09:00 +0100

    Repasando ejercicios del Tema 7

--- Tema 7/Ejercicio04.java
+++ Tema 7/Ejercicio04.java
@@ -1,42 +1,39 @@
 package tema07;
 
 /**
- * 
- * Ejercicio 4
  *
  * @author Juan Luis Aranda
  */
 public class Ejercicio04 {
   public static void main(String[] args) {
-    //Creo los tres arrays
-	int[] numero = new int[20];
-	int[] cuadrado = new int[20];
-	int[] cubo = new int[20];
-	///////////////////////
-	
-	//Para el array números hago un bucle que almacene datos aleatorios
-	for(int i = 0; i <=19;i++){
-		numero[i] = (int)(Math.random()*100);
-		System.out.println("El número generado en el hueco "+i+" es: "+numero[i]);
-	}
-	///////////////////////
-	
-	System.out.println("--------------------------------------------------------");
-	
-	//Para el array cuadrado cojo el array números 
-	for(int j = 0; j <=19;j++){
-		cuadrado[j] = numero[j] * numero[j];
-		System.out.println("El cuadrado del número del hueco " + j + " es: " + cuadrado[j]);
-	}
-	//////////////////////
-	
-	System.out.println("--------------------------------------------------------");
-	
-	//Para el array cubo cojo el array números 
-	for(int k = 0; k <=19;k++){
-		cubo[k] = numero[k] * numero[k] * numero[k];
-		System.out.println("El cubo del número del hueco " + k + " es: " + cubo[k]);
+    // Creamos los 3 arrays
+    int[] numero = new int[20];
+    int[] cuadrado = new int[20];
+    int[] cubo = new int[20];
+    
+    // Creamos un bucle que genere números aleatorio para el primer array
+    for(int i = 0; i < 20; i++){
+      numero[i] = (int)(Math.random()* 100 ) + 1; // Math.random es como si fuera 0.9999 sin llegar a ser uno, por lo que hay que sumarle 1, así lo genera del 1 al 100 ambos incluidos
+      System.out.println("El numero generado en el hueco " + i + " es: " + numero[i]);
     }
-//////////////////////
+    
+    System.out.println("--------------------------------------------------------");
+    
+    
+    //Para el array cuadrado cojo el array numero
+    for(int j = 0; j < 20; j++){
+      cuadrado[j] = numero[j] * numero[j];
+      System.out.println("El número generado en el hueco " + j + " es: " + cuadrado[j]);
+    }
+    
+    System.out.println("--------------------------------------------------------");
+    
+    
+    // De nuevo, vuelvo a coger el array numero multiplicado por 3 para que sea el cubo
+    for(int k = 0; k < 20; k++){
+      cubo[k] = numero[k] * numero[k] * numero[k];
+      System.out.println("El número generado en el hueco " + k + " es: " + cubo[k]);
+    }
   }
+  
 }
