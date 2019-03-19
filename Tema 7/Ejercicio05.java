6ee01b26de6949e17a79ef347349f92c7ca9bca6
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2019-03-19 10:52:26 +0100

    Repasando ejercicios del Tema 7

--- Tema 7/Ejercicio05.java
+++ Tema 7/Ejercicio05.java
@@ -3,55 +3,52 @@ package tema07;
 import java.util.Scanner;
 
 /**
- * 
- * Ejercicio 5
  *
- * @author Juan Lusi Aranda
+ * @author Juan Luis Aranda
  */
 public class Ejercicio05 {
-  public static void main(String[] args) {
-    //Creo el array
-		Scanner s = new Scanner(System.in);
-		int[] miArray = new int[10];
-		
-		//Lee los datos
-		
-		for(int i = 0; i<=9;i++){
-			System.out.println("Introduce el numero: ");
-			String linea = (s.nextLine());		
-			miArray[i]= Integer.parseInt( linea ); 		
-			
-		}
-		System.out.println("-----------------------");
-		
-		//Calcular el max y min 
-		int maximo = miArray[0];
-		int minimo = miArray[0];
-		
-		for(int elemento : miArray){ //Otra forma para recorrer un array
-			if(elemento < minimo){
-				minimo = elemento;
-			}
-			
-			if(elemento > maximo){
-				maximo = elemento;
-			}
-		}
-		
 
-				
-		//Muestro los resultados		
-		for(int i = 0; i<=9;i++){
-			System.out.print(miArray[i]);
-			
-			if (miArray[i]==minimo){
-				System.out.print(" minimo ");
-			}
-			
-			if (miArray[i]==maximo){
-				System.out.print(" maximo ");
-			}
-			System.out.println("");			
+  public static void main(String[] args) {
+    // Creamos el array
+    Scanner s = new Scanner(System.in);
+    int[] miArray = new int[10];
+
+    //Pide los numeros y los lee
+    for (int i = 0; i < 10; i++) {
+      System.out.print("Introduce un número: ");
+      String linea = (s.nextLine());
+      miArray[i] = Integer.parseInt(linea);
+
     }
-  } 
+
+    System.out.println("----------------------------");
+
+    // Calculamos el máximo y el mínimo
+    int maximo = miArray[0];
+    int minimo = miArray[0];
+
+    for (int elemento : miArray) { // Esto es otra forma de recorrer un array
+      if (elemento < minimo) {
+        minimo = elemento;
+      }
+
+      if (elemento > maximo) {
+        maximo = elemento;
+      }
+    }
+
+    // Mostramos los resultados
+    for (int i = 0; i < 10; i++) {
+
+      if (miArray[i] == minimo) {
+        System.out.println(miArray[i] + " (mínimo) ");
+      
+      }else if (miArray[i] == maximo){
+      System.out.println(miArray[i] + " (máximo) ");
+      } else{
+        System.out.println(miArray[i]);
+      }
+    }
+
+  }
 }
