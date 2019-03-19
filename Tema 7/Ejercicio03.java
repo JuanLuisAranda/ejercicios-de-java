6ddd9c557ece06f9c2b33dd6969f17306690c88d
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2019-03-19 09:38:07 +0100

    Repasando ejercicios del Tema 7

--- Tema 7/Ejercicio03.java
+++ Tema 7/Ejercicio03.java
@@ -3,31 +3,27 @@ package tema07;
 import java.util.Scanner;
 
 /**
- * 
- * Ejercicio 3
  *
  * @author Juan Luis Aranda
+ *
  */
 public class Ejercicio03 {
   public static void main(String[] args) {
-    //Creo los tres arrays
-	
-	
-	Scanner s = new Scanner(System.in);
-	int[] miarray = new int[10];
-	
-	for(int i = 0; i<=9;i++) {
-		String linea;
-		System.out.println("Introduce el número ");
-		linea = (s.nextLine());
-		miarray[i]= Integer.parseInt( linea ); 		
-		}
-	System.out.println("-----------------------");
-	
-	for(int j = 9; j>=0;j--){
-		System.out.println("En el hueco "+j+" tengo el número "+miarray[j]);
+    
+    Scanner s = new Scanner(System.in);
+    
+    int [] miarray = new int[10];
+    
+    for(int i = 0; i < 10; i++){
+      String linea;
+      System.out.print("Introduce un número: ");
+      linea = (s.nextLine());
+      miarray[i] = Integer.parseInt(linea);
     }
-    /////////////////
+    
+    for(int j = 9; j >= 0; j--){
+      System.out.println("En el hueco "+ j + " tengo el número " + miarray[j]);
+    }
   }
   
 }
