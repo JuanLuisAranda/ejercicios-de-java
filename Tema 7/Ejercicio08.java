43b1b0895ec9ffe19afb12ee633deff4365af5a8
JuanLuisAranda <jl.aranda.navarro@hotmail.com>
2019-03-20 14:00:56 +0100

    Repasando ejercicios del Tema 7

--- Tema 7/Ejercicio08.java
+++ Tema 7/Ejercicio08.java
@@ -3,37 +3,35 @@ package tema07;
 import java.util.Scanner;
 
 /**
- * 
- * Ejercicio 8
  *
  * @author Juan Luis Aranda
  */
 public class Ejercicio08 {
+
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-
-    System.out.println("Este programa muestra en pantalla la media de las");
-    System.out.println("temperaturas a lo largo de un año.");
+    
     int mes = 1;
     float[] temperatura = new float[12];
-    for (int i = 0; i < 12; i++) {
-
-      System.out.print("Dime la temperatura del mes " + mes + ": ");
-      temperatura[i] = Float.parseFloat(s.nextLine());
-      mes++;
-    }
+    for  (int i = 0; i < 12; i++) {
+    System.out.print("Dime la temperatura del mes " + mes + ": ");
+    temperatura[i] = Float.parseFloat(s.nextLine());
+    mes++; 
+  }
     System.out.println();
     for (int i = 0; i < 12; i++) {
-      if (i < 9) {
-        System.out.print(i + 1 + ":  ");
-      } else {
-        System.out.print(i + 1 + ": ");
-      }
+//      if (i < 9) {
+//        System.out.print(i + 1 + ":  ");
+//      } else {
+//        System.out.print(i + 1 + ": ");
+//      }
+        System.out.printf("%2d: ", i); // Reserva 2 huecos
       for (int j = 0; j < temperatura[i] / 2; j++) {
         System.out.print("█");
       }
       System.out.println();
     }
+    
   }
   
 }
